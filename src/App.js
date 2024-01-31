385637086cbd9328ae819dd973704e0656ddf9d7
sagini18 <saginisaju@gmail.com>
2024-01-31 09:58:33 +0530

    a basic web page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button className='App-button'>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://github.com/apps/pr-comment-on-build/installations/new"
           rel="noopener noreferrer"
         >
-          Learn React
+        <img 
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/1200px-GitHub_Invertocat_Logo.svg.png'
+        className="App-logo" alt="logo" />
+          Authorize With GitHub
         </a>
+        </button>
       </header>
     </div>
   );
