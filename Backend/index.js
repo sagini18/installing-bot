d78b311c9da07fd869af9b44870d548c270c54c8
sagini18 <saginisaju@gmail.com>
2024-02-03 11:58:04 +0530

    frontend and backend integration

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,16 @@
 import  express from "express";
 import pullRequestHandler from "./routes/pullRequestHandler.js";
 import auth from "./routes/auth.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", pullRequestHandler);
 app.use("/github-app",auth);
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
 });
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,10 +3,11 @@ import axios from "axios";
 
 const auth = Router();
 
-auth.get("/authorize", (req, res) => {
-  const redirectURL = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=repo`;
-  res.redirect(redirectURL);
-});
+//// if authorization is required from server side only
+// auth.get("/authorize", (req, res) => {
+//   const redirectURL = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=repo`;
+//   res.redirect(redirectURL);
+// });
 
 auth.get("/callback", (req, res) => {
   const tokenURL = `https://github.com/login/oauth/access_token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.query.code}`;
